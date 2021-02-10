e9d4571da59dd65545bc46fba4ae89bd32bc15c9
J. Nick Koston <nick@koston.org>
2021-02-09 20:19:23 -1000

    Fix version

--- setup.py
+++ setup.py
@@ -4,45 +4,49 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="J. Nick Koston",
-    author_email='nick@koston.org',
-    python_requires='>=3.5',
+    author_email="nick@koston.org",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Async for Oncue",
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='aiooncue',
-    name='aiooncue',
-    packages=find_packages(include=['aiooncue', 'aiooncue.*']),
+    keywords="aiooncue",
+    name="aiooncue",
+    packages=find_packages(include=["aiooncue", "aiooncue.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/bdraco/aiooncue',
-    version='version="0.2.0"',
+    url="https://github.com/bdraco/aiooncue",
+    version="0.2.0",
     zip_safe=False,
 )
