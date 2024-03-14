e63c3be1334bb96d97306d9ff429c46074f62462
J. Nick Koston <nick@koston.org>
2024-03-14 09:39:03 -1000

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/bdraco/aiooncue",
-    version="0.3.6",
+    version="0.3.7",
     zip_safe=False,
 )
